1e3dc2a1a51236300f9fceef82e991dd81fa8006
James McNeil <mcneil_james@hotmail.com>
2020-02-10 14:29:42 +0000

    typo for screen colour

--- coin/coin.py
+++ coin/coin.py
@@ -12,7 +12,7 @@ coin = Actor("coin")
 coin.pos = 200, 200
 
 def draw():
-    screen.fill("gree")
+    screen.fill("green")
     fox.draw()
     coin.draw()
     screen.draw.text("Score: " + str(score), color="black", topleft=(10,10))
