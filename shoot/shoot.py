5e5bffddae72361d3db2e945e628be41a132d8be
James McNeil <mcneil_james@hotmail.com>
2020-02-10 12:10:17 +0000

    adding ball to game

--- shoot/shoot.py
+++ shoot/shoot.py
@@ -1,14 +1,19 @@
 from random import randint
 
 apple = Actor("apple")
+ball = Actor("ball")
 score = 0
-timeer = 10
+timer = 10
 
 def draw():
     global score
     screen.clear()
     apple.draw()
     screen.draw.text("Score: " + str(score), topleft=(10,10))
+
+def place_ball():
+    ball.x = randint(10,800)
+    ball.y = randint(10,600)
 
 def place_apple():
     apple.x = randint(10,800)
@@ -21,6 +26,13 @@ def on_mouse_down(pos):
         print("Good shot!")
         screen.draw.text("Score: " + str(score), topleft=(10,10))
         print("Score: {0}".format(score))
+        place_apple()
+        place_ball()
+    elif ball.collidepoint(pos):
+        score = score - 2
+        print("Oof, wrong item dude!!!")
+        screen.draw.text("Score: "+ str(score), topleft=(10,10))
+        place_ball()
         place_apple()
     else:
         print("You missed!")
